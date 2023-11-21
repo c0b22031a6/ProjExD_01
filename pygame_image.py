7abf0260b1e77146dc48647a5d4356e9eaa5af8d
c0b22031 <c0b22031a6@edu.teu.ac.jp>
2023-11-21 15:58:01 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -15,11 +15,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0]) #練習4
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0]) #練習4
+        screen.blit(bg_img, [1600-x, 0])
         screen.blit(kk_img[tmr%2], [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
